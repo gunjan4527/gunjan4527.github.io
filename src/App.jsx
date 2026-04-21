6e7fa524ba6396660bd547cefd9866607e41901b
gunjan4527 <ygunjankumar3@gmail.com>
2026-04-21 15:45:56 +0530

    update portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import React, { useState, useEffect } from "react";
-import { Helmet } from "react-helmet";
-
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Skills from "./components/Skills";
@@ -26,26 +24,6 @@ function App() {
 
   return (
     <div className="App" id="top">
-
-      {/* ✅ Google Analytics */}
-      <Helmet>
-        {/* Load gtag */}
-        <script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=G-96NZBCQE7L"
-        ></script>
-
-        {/* Initialize GA */}
-        <script>
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-96NZBCQE7L');
-          `}
-        </script>
-      </Helmet>
-
       <Navbar toggleTheme={toggleTheme} darkMode={darkMode} />
 
       <Hero />
@@ -62,74 +40,3 @@ function App() {
 }
 
 export default App;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import React, { useState, useEffect } from "react";
-// import { Helmet } from "react-helmet";
-
-// import Navbar from "./components/Navbar";
-// import Hero from "./components/Hero";
-// import Skills from "./components/Skills";
-// import Resume from "./components/Resume";
-// import Projects from "./components/Projects";
-// import Experience from "./components/Experience";
-// import Education from "./components/Education";
-// import ChartSection from "./components/ChartSection";
-// import Chartbot from "./components/Chatbot";
-// import Contact from "./components/Contact";
-
-// function App() {
-//   const [darkMode, setDarkMode] = useState(true);
-
-//   useEffect(() => {
-//     const theme = darkMode ? "dark" : "light";
-//     document.body.className = theme;
-//   }, [darkMode]);
-
-//   const toggleTheme = () => {
-//     setDarkMode(prev => !prev);
-//   };
-
-//   return (
-//     <div className="App" id="top">
-
-//       <Navbar toggleTheme={toggleTheme} darkMode={darkMode} />
-
-//       <Hero />
-//       <Skills />
-//       <Projects />
-//       <Experience />
-//       <Resume />
-//       <Education />
-//       <ChartSection />
-//       <Contact />
-//       <Chartbot />
-//     </div>
-//   );
-// }
-
-// export default App;
