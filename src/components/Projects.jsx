cddd3eaa6603db3f45476248fa928526988cde6c
gunjan4527 <ygunjankumar3@gmail.com>
2026-04-27 21:39:30 +0530

    update portfolio

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -13,7 +13,7 @@ export default function Projects() {
     {
       title: "Pizza Sales Analysis",
       desc: "Analyzed pizza sales data to find trends and improve business decisions.",
-      github: "https://github.com/gunjan4527/pizza-analysis",
+      github: "https://github.com/gunjan4527/pizza-sales-analysis",
       linkedin: "https://www.linkedin.com/in/gunjan-kumar-5866042aa",
       tech: "SQL, Power BI"
     },
@@ -27,7 +27,7 @@ export default function Projects() {
     {
       title: "COVID-19 Analysis",
       desc: "Analyzed COVID-19 dataset to visualize trends and insights.",
-      github: "https://github.com/gunjan4527/covid-analysis",
+      github: "https://github.com/gunjan4527/Covid-19-Data-Analysis",
       linkedin: "https://www.linkedin.com/in/gunjan-kumar-5866042aa",
       tech: "Python, Pandas, Matplotlib"
     },
@@ -41,14 +41,14 @@ export default function Projects() {
     {
       title: "Banking Domain",
       desc: "Analyzed a banking dataset of 3,000 clients using Python and Power BI.",
-      github: "https://github.com/gunjan4527/spotify-analysis",
+      github: "https://github.com/gunjan4527/Banking-Domain",
       linkedin: "https://www.linkedin.com/in/gunjan-kumar-5866042aa",
       tech: "Python, Data Visualization, Power BI"
     },
     {
       title: "Customer Behavior Analysis",
       desc: "Data analytics project showcasing customer behavior analysis using python sql and power bi",
-      github: "https://github.com/gunjan4527/spotify-analysis",
+      github: "https://github.com/gunjan4527/Customer-Behavior-Analysis",
       linkedin: "https://www.linkedin.com/in/gunjan-kumar-5866042aa",
       tech: "Python, Data Visualization, SQL, Power BI"
     },
@@ -62,21 +62,21 @@ export default function Projects() {
     {
       title: "IMDb top 1000 Movies",
       desc: "IMDb Top 1000 Movies Dashboard Using Tableau",
-      github: "https://github.com/gunjan4527/spotify-analysis",
+      github: "https://github.com/gunjan4527/IMDB-Tableau-dashboard",
       linkedin: "https://www.linkedin.com/in/gunjan-kumar-5866042aa",
       tech: "Tableau"
     },
     {
       title: "Medical Context-Based Chatbot (RAG)",
       desc: "Built a RAG-based medical chatbot for context-aware question answering",
-      github: "https://github.com/gunjan4527/spotify-analysis",
+      github: "https://github.com/gunjan4527/Medical-Context-Based-Chatbot-RAG-",
       linkedin: "https://www.linkedin.com/in/gunjan-kumar-5866042aa",
       tech: "Python 3.10, Machine Learning /NLP,LLM & Embeddings,Backend & API,Frontend / UI"
     },
     {
       title: "AI-powered Crop Recommendation System",
       desc: "Crop Recommendation System that helps farmers choose the most suitable crops based on soil nutrients and environmental conditions.",
-      github: "https://github.com/gunjan4527/spotify-analysis",
+      github: "https://github.com/gunjan4527/Crop-Recoomendation-System",
       linkedin: "https://www.linkedin.com/in/gunjan-kumar-5866042aa",
       tech: "Python, ML,Pandas, NumPy, EDA,Flask (REST API)"
     },
